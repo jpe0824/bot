5d69c4d731b502b0c5300d8f220a913723c95c68
jpe0824 <jason.e24@gmail.com>
2023-04-20 22:41:15 -0600

    Finished die command

---
+++ commands/die.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require("discord.js");
+require("dotenv").config(); //to start process from .env file
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("die")
+    .setDescription("Shuts down the server.")
+    .addStringOption((option) =>
+      option
+        .setName("password")
+        .setDescription("The password required to shut down the server.")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const password = interaction.options.getString("password");
+
+    if (password !== process.env.PASSWORD) {
+      return interaction.reply("Invalid password. Access denied.");
+    }
+
+    await interaction.reply("Goodbye cruel world!");
+
+    process.exit();
+  },
+};
